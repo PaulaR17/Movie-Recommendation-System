447ec12f7f8bb1edc06d06fec43528318a894462
PaulaR17 <paularomerogallart@gmail.com>
2024-12-22 00:23:51 +0100

    Generated TF-IDF matrix with 10,000 features for recommendation model

---
+++ preprocesing.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import re #regular expression operations
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import nltk
+
+df=pd.read_csv("./CSV/peliculas.csv") #cargamos el archivo csv
+print (df.describe(include='all')) #que muestre todo
+print (df.info()) #miramos el nombre de las columnas, tipos da datos, valores que faltan...
+
+df_preprocess = df.copy() #se edita esta copia de los datos para hacer to do el preprocesado para el modelo
+#dividimos las columnas en no numericas y númericas
+numeric_columns=df_preprocess.select_dtypes(include='number').columns  #columnas numericas
+non_numeric_columns=df_preprocess.select_dtypes(exclude='number').columns #columnas no numéricas
+
+#estudio de NULLs, miramos que porcentaje de nullos tiene cada columna
+null_percentage=df_preprocess.isnull().sum()/len(df_preprocess.index)*100
+print ("Porcentaje de NULLs por culumna: ",null_percentage)
+
+#rellenamos todos los NULLs con "unknown"
+df_preprocess = df_preprocess.fillna("Unknown")
+
+#descargamos los resucrsos necesarios de nltk (solo pasará una vez)
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+#empezamos el procesamiento de texto
+
+#columnas no numéricas
+stop_words = set(stopwords.words('english')) #cargamos unas stopwords tipics del ingles (the,and,is...) y las convertimos en un conjunto. Lo hacemos para reducir el ruido en el texto
+lemmatizer = WordNetLemmatizer() #inicializamos un lematizador, que es la herramienta que reduce las palabras a su forma base.
+
+def preprocess_text(text):
+    text=text.lower() #convertimos to do el texto en minúsculas
+    text=re.sub(r'[^\w\s\d]','',text) #eliminamos cualquier carácter que no sea una letra (\w) o un espacio (\s) o un número (\d) asi nos quitamos los simbolos de puntuacion o caracteres especiales
+    tokens=text.split() #dividimos el texto en palabras individuales
+    tokens=[word for word in tokens if word not in stop_words] #eliminamos stop words
+    tokens=[lemmatizer.lemmatize(word) for word in tokens] #lematizamos cada palabra
+    return ' '.join(tokens) #unimos todas las palabras procesadas en una sola cadena
+
+for col in non_numeric_columns:
+    df_preprocess[col] = df_preprocess[col].apply(preprocess_text) #aplicamos el procesamiento de texto a cada celda de las columnas no numéricas
+
+df_preprocess['combined_text'] = df_preprocess.apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
+
+df_preprocess.to_csv("./CSV/preprocessed_peliculas.csv", index=False) #guardamos el dataset procesado
+
+print ("PreProcesamiento COMPLETADO YAY!")
---
+++ vectorization.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer #convertir texto en valores numéricos basandonos en la frecuencia de palabras (TF-IDF)
+from preprocesing import df_preprocess
+
+df_preprocess = pd.read_csv("./CSV/preprocessed_peliculas.csv") #leemos el datafreme preprocesado que hemos creado en el script postprocesing
+
+tfidf_vectorizer = TfidfVectorizer(max_features=10000) #creamos una instancia de tfidvectorizer y le ponemos que solo coja las 10.000 palabras más importantes para optimizar
+tfidf_matrix = tfidf_vectorizer.fit_transform(df_preprocess['combined_text'])
+"""
+.fit --> aprende el vocabulario del texto en la columna combined_text y calcula el IDF para las palabras
+transform --> convierte el texto en vectores numéricos basados en TF-IDF
+RESULTADO --> matriz dispersa, cada fila representa una película y cada columna una palabra
+"""
+tfidf_df=pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
+"""
+tfidf_matrix.toarray --> convierte la matriz en una matriz densa, que se puede convertir en un dataframe
+columns=tfidf_vectorizer.get_feature_names_out --> obtiene las caracteristicas de las palabras que represetnan las columnas del dataframe
+cada fila correspone a una peli y cada columna tiene el peso TF-IDF de una palabra en esa pelicula
+"""
+tfidf_df.to_csv("./CSV/tfidf_matrix_combined.csv", index=False)
+
